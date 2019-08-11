35bc41f4b71dca7bdd91d398c63e4c4c6cf40f85
glnse_ <glar.tirao@up.phinma.edu.ph>
2019-08-11 14:45:23 +0800

    Added DELETE Request

--- api/post.php
+++ api/post.php
@@ -3,6 +3,8 @@
 
 include('../config/connection.php');
 
+header('Content-Type: application/json');
+
 // FETCHING POST
 if ($_SERVER['REQUEST_METHOD']=="GET"){
     if (isset($_GET['article_id'])){
@@ -14,10 +16,9 @@ if ($_SERVER['REQUEST_METHOD']=="GET"){
     }
     $result = $conn->query($sql);
     if ($result->num_rows > 0){
-        $response = array();
         while($row = $result->fetch_assoc()){
             $user_id = $row['user_id'];
-            $getUserSql = "SELECT id,fname,lname FROM op_users WHERE id='$user_id' LIMIT 1";
+            $getUserSql = "SELECT id,fname,lname,course,dept FROM op_users WHERE id='$user_id' LIMIT 1";
             $getUserResult = $conn->query($getUserSql) or die($conn->error());
             if ($getUserResult->num_rows > 0){
                 unset($row['user_id']);
@@ -26,17 +27,12 @@ if ($_SERVER['REQUEST_METHOD']=="GET"){
             }
             $response[] = $row;
         }
-        //print_r($response);
-        header('Content-Type: application/json');
-        $json = json_encode($response);
-        echo $json;
     }
     else {
-        header('Content-Type: application/json');
         $response = array("message" => "No News Available", "status" => "no_content");
-        $json = json_encode($response);
-        echo $json;
     }
+    $json = json_encode($response);
+    echo $json;
 }
 
 // NEW POST
@@ -54,16 +50,57 @@ else if ($_SERVER['REQUEST_METHOD']=="PUT"){
     parse_str($data,$_PUT);
     $json = json_encode($_PUT);
     echo $json;
-    echo $json;
 }
 
 // DELETE POST
 else if ($_SERVER['REQUEST_METHOD']=="DELETE"){
-    header('Content-Type: application/json');
+    // GET DELETE REQUEST DATA
     $data = file_get_contents('php://input');
     $_DELETE = array();
     parse_str($data,$_DELETE);
-    $json = json_encode($_DELETE);
+
+    if ((isset($_DELETE['article_id']))&&(isset($_DELETE['access_token']))){
+        $access_token = $_DELETE['access_token'];
+        $article_id = $_DELETE['article_id'];
+
+        $sql = "SELECT * FROM op_articles WHERE id='$article_id' LIMIT 1";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0){
+            $article = $result->fetch_assoc();
+            $sql = "SELECT * FROM op_users WHERE token='$access_token' LIMIT 1";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $user = $result->fetch_assoc();
+                if ($user['id']==$article['user_id']){
+                    $response = array("message" => "Deleted Successfully", "status" => "success_delete");
+                }
+                else {
+                    $response = array("message" => "Denied Access", "status" => "no_access");
+                }
+            }
+            else {
+                $response = array("message" => "Denied Access", "status" => "no_access");
+            }
+        }
+        else {
+            $response = array("message" => "No Article Found", "status" => "no_article");
+        }
+    }
+
+    else {
+        if (isset($_DELETE['article_id'])){
+            $response = array("message" => "Required Access Token", "status" => "no_access_token");
+        }
+        else if (isset($_DELETE['access_token'])){
+            $response = array("message" => "Required Article ID", "status" => "no_target_article_id");
+        }
+        else {
+            $response = array("message" => "Required Parameters (article_id,access_token)", "status" => "no_target_article_id");
+        }
+    }
+    $json = json_encode($response);
     echo $json;
 }
+
+$conn->close();
 ?>
